e44759b2e4091f823689132f43d368ee17df9b8d
flo-314 <julianflo1916@gmail.com>
2021-10-19 16:16:00 -0300

    .

--- src/index.js
+++ src/index.js
@@ -3,7 +3,38 @@ const itemFactory = (title, description, checklist, priority, dueDate) => {
 };
 
 let array = [];
+const mainMethods = (() => {
+  function deleteMain(){
+    const main = document.querySelector("main")
+    main.innerHTML = ""
+  }
+  function printMain(){
+    const main = document.querySelector("main")
+    deleteMain()
+    domItemMethods.printArray()
+  }
+  return{printMain,deleteMain}
+})()
 
+
+const sidebarMethods = (() => {
+  const homeBtn = document.querySelector("#homeBtn");
+  homeBtn.addEventListener("click", () => {
+
+  mainMethods.printMain()
+    
+  });
+
+  const todayBtn = document.querySelector("#todayBtn");
+  todayBtn.addEventListener("click", () => {
+
+  });
+
+  const weekBtn = document.querySelector("#weekBtn");
+  weekBtn.addEventListener("click", () => {
+
+  });
+})();
 
 const promptMethods = (() => {
   createItemBtn();
@@ -39,10 +70,9 @@ const promptMethods = (() => {
     itemDescription.type = "text";
     itemDescription.placeholder = "Description";
     itemDescription.classList.add("itemDescription");
-    
-    const dueDate = document.createElement("input")
-    duedate.type = "date"
 
+    const dueDate = document.createElement("input");
+    dueDate.type = "date";
 
     const sumbitBtn = document.createElement("button");
     sumbitBtn.textContent = "Add new Task!";
@@ -50,75 +80,73 @@ const promptMethods = (() => {
     sumbitBtn.addEventListener("click", () => {
       let descriptionValue = itemDescription.value;
       let titleValue = itemTitle.value;
-      let dateValue = duedate.value
+      let dueDateValue = dueDate.value;
 
-
-
-
-      itemMethods.createItem(titleValue, descriptionValue,dateValue);
-      domItemMethods.printItem(titleValue,descriptionValue,dateValue)
+      itemMethods.createItem(titleValue, descriptionValue, dueDateValue);
+      domItemMethods.printItem(titleValue, descriptionValue, dueDateValue);
       removePromp();
       createItemBtn();
     });
 
     const main = document.querySelector("main");
-    addItemContainer.append(itemTitle, itemDescription,dueDate, sumbitBtn);
+    addItemContainer.append(itemTitle, itemDescription, dueDate, sumbitBtn);
     main.append(addItemContainer);
   }
   return { itemPrompt };
 })();
 
-
 const domItemMethods = (() => {
-  function printArray (){
-  array.forEach(item => {
-      printItem()
+  function printArray() {
+    itemMethods.homeArray.forEach((item) => {
+      printItem();
     });
-
   }
-  function printItem(title,description,duedate,checklist) {
-    const itemContainer = document.querySelector("#item-container")
-    const item = document.createElement("div")
-    item.classList.add("item")
-    const leftContainer = document.createElement("div")
-    leftContainer.classList.add("left-container")
-    const itemTitle = document.createElement("div")
-    itemTitle.classList.add("item-title")
-    const itemDescription = document.createElement("div")
-    itemDescription.classList.add("item-description")
-    const rightContainer = document.createElement("div")
-    rightContainer.classList.add("right-container")
-    const itemCheckList = document.createElement("div")
-    itemCheckList.classList.add("item-checklist")
-    const itemDueDate = document.createElement("div")
-    itemDueDate.classList.add("item-duedate")
-    const hr = document.createElement("hr")
+  function printItem(title, description, duedate, checklist) {
+    const itemContainer = document.querySelector("#item-container");
+    const item = document.createElement("div");
+    item.classList.add("item");
+    const leftContainer = document.createElement("div");
+    leftContainer.classList.add("left-container");
+    const itemTitle = document.createElement("div");
+    itemTitle.classList.add("item-title");
+    const itemDescription = document.createElement("div");
+    itemDescription.classList.add("item-description");
+    const rightContainer = document.createElement("div");
+    rightContainer.classList.add("right-container");
+    const itemCheckList = document.createElement("div");
+    itemCheckList.classList.add("item-checklist");
+    const itemDueDate = document.createElement("div");
+    itemDueDate.classList.add("item-duedate");
+    const hr = document.createElement("hr");
 
+    itemTitle.textContent = title;
+    itemDescription.textContent = description;
+    itemDueDate.textContent = duedate;
 
-    itemTitle.textContent = title
-    itemDescription.textContent = description
-    itemDueDate.textContent = duedate
-
-    leftContainer.append(itemTitle,itemDescription)
-    rightContainer.append(itemCheckList,itemDueDate)
-    item.append(leftContainer,rightContainer,hr)
-    itemContainer.appendChild(item)
-  } 
-
-function expandItem(){
-
- }
-  return{printArray,printItem}
-
- })();
-
-const itemMethods = (() => {
-  function createItem(title, description, duedate) {
-    let newItem = itemFactory(title, description, duedate);
-    array.push(newItem);
-    console.log(array);
+    leftContainer.append(itemTitle, itemDescription);
+    rightContainer.append(itemCheckList, itemDueDate);
+    item.append(leftContainer, rightContainer, hr);
+    itemContainer.appendChild(item);
   }
 
-  return { createItem };
+  function expandItem() {}
+  return { printArray, printItem };
 })();
 
+const itemMethods = (() => {
+  let homeArray = [{
+    "title": "asdasd",
+    "description": "asdasd",
+    "checklist": "2021-10-04"
+  }];
+  let todayArray = [];
+  let weekArray = [];
+
+  function createItem(title, description, dueDate, checkbox, array) {
+    let newItem = itemFactory(title, description, dueDate, checkbox, array);
+    homeArray.push(newItem);
+    console.log(homeArray);
+  }
+
+  return { createItem, homeArray };
+})();
