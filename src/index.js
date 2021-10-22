5a776a3ea4e7dfa1184374e818e4fa841d56473b
flo-314 <julianflo1916@gmail.com>
2021-10-22 15:06:33 -0300

    buttons css fixed

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import { sidebarMethods, domProjectMethods } from "./domSideBar";
 import { itemMethods } from "./items.js";
 import { projectMethods } from "./projects.js";
 
-itemMethods.deleteItem("Home","sdf")
-mainMethods.printMain("Home");
 domProjectMethods.printStoredProjects();
+mainMethods.printMain("Home");
+
 //to do
 /* }
     ----- SIstema de prioridades | ordenNumeral   /..?? supongo que por un lado tendria que modificar en el itemfactory y por el otro lado en el Dom Item Methods + el read
     ----- local storage                                   /storage.js
-    ------- no funciona bien ni funciona en el home por como esta hecho.
+    ------- no funciona bien el week por fechas ni el today por fechas. ni funciona en el home por como esta hecho.
  */
